13fc8a8cfe14578f2fd3f904f3a57e68305a060a
MALWICK <batahumphrey66@gmail.com>
2024-03-28 08:07:53 +0100

    updating database baseurl path

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose")
 const dbConnect = () => {
     try {
         // const connect = mongoose("mongodb+srv://admin:Sr83850G9iV8bZlF@cluster0.24hjfhx.mongodb.net/")
-        const conn = mongoose.connect('mongodb://localhost:27017')
+        const conn = mongoose.connect(process.env.MONGODB_URL)
         console.log("connecting to mongoose successfully")
     } catch (error) {
         console.log(error, "Couldn't connect to Mongo");
