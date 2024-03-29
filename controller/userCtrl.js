fed7e0a93f1f9da2a57577a2456935ae48cc4f75
MALWICK <batahumphrey66@gmail.com>
2024-03-29 14:17:33 +0100

    made first post request

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,15 +1,35 @@
 const User = require("../models/userModel");
 
-const createUser = async (req,res) => {
-    const email = req.body.email;
-    const findUser = await User.findOne({ email: email });
-    if (!findUser) {
-        //create a new user
-        const newUser = User.create(req.body);
-        res.json(newUser);
-    }else{
-        msg:"User already exists"
-        success:false
-        //user already exists
+const createUser = async (req, res) => {
+  try {
+    const { firstname, lastname, email, mobile, password } = req.body;
+
+    if (!firstname || !lastname || !email || !mobile || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
-}
+
+    const findUser = await User.findOne({ email: email });
+
+    if (!findUser) {
+      // Create a new user
+      const newUser = await User.create(req.body);
+      res.json(newUser);
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+module.exports = createUser;
