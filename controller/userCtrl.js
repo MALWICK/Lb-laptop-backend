79dffa9ccbb655fb624583e3c70c4dcb424ebdaa
MALWICK <batahumphrey66@gmail.com>
2024-03-29 11:44:05 +0100

    updating code

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -5,7 +5,11 @@ const createUser = async (req,res) => {
     const findUser = await User.findOne({ email: email });
     if (!findUser) {
         //create a new user
+        const newUser = User.create(req.body);
+        res.json(newUser);
     }else{
+        msg:"User already exists"
+        success:false
         //user already exists
     }
 }
