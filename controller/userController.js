161226d4c2773dd407734929ea3b51c352f69b4c
MALWICK <batahumphrey66@gmail.com>
2024-03-30 18:11:32 +0100

    updaating login user

--- controller/userController.js
+++ controller/userController.js
@@ -32,32 +32,6 @@ exports.register = async (req, res) => {
   }
 };
 
-// exports.login = async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     // Check if the user exists
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//       return res.status(404).json({ message: 'User not found' });
-//     }
-
-//     // Check if the password is correct
-//     const isPasswordValid = await bcrypt.compare(password, user.password);
-//     if (!isPasswordValid) {
-//       return res.status(401).json({ message: 'Invalid password' });
-//     }
-
-//     // Generate a JWT token for session management
-//     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-//     // Send the token in the response
-//     res.json({ message: 'Login successful', token });
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error logging in', error: error.message });
-//   }
-// };
-
 exports.login = async (req, res) => {
   const { email, password } = req.body;
 
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const dotenv = require('dotenv').config();
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 dbConnect()
+const authMiddleware = require("./middlewares/authMiddleware");
 
 
 app.use(express.json());
@@ -14,3 +15,7 @@ app.use("/api/user", authRouter);
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
 });
+app.get('/protected-route', authMiddleware, (req, res) => {
+    // Access the userId from req.userId
+    // Perform actions that require authentication
+  });
